711d8ff7de1a44f3f086c4c426f330d8152f326c
mctasoglu <65802446+mctasoglu@users.noreply.github.com>
2020-07-12 16:03:54 -0400

    Updates

---
+++ precache-manifest.4464a3b06d19efbdb7066b7efb884b7e.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ab724044386a073ffe6d25e681cac7",
+    "url": "/MSA/index.html"
+  },
+  {
+    "revision": "91c0d0241009f7dd41ff",
+    "url": "/MSA/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "f69ee703716ea6598ed6",
+    "url": "/MSA/static/css/main.e6df9666.chunk.css"
+  },
+  {
+    "revision": "91c0d0241009f7dd41ff",
+    "url": "/MSA/static/js/2.7e912b30.chunk.js"
+  },
+  {
+    "revision": "b396b9c7cb89986c0edcd31803c9a394",
+    "url": "/MSA/static/js/2.7e912b30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f69ee703716ea6598ed6",
+    "url": "/MSA/static/js/main.14cd567d.chunk.js"
+  },
+  {
+    "revision": "cfb9f809907e425af856",
+    "url": "/MSA/static/js/runtime-main.0d19cfc3.js"
+  },
+  {
+    "revision": "b5b4137690e565d7c0fc25119ed63aae",
+    "url": "/MSA/static/media/aidan.b5b41376.jpg"
+  },
+  {
+    "revision": "24f59f839134e76c63eeaa5579ae90be",
+    "url": "/MSA/static/media/ali.24f59f83.jpg"
+  },
+  {
+    "revision": "5e416260055c7bef2f25187e0d52c5f1",
+    "url": "/MSA/static/media/azaan.5e416260.jpg"
+  },
+  {
+    "revision": "16e998ef6f9e27c2b4a3359a3d3a700c",
+    "url": "/MSA/static/media/basmala.16e998ef.png"
+  },
+  {
+    "revision": "baa6c7d2f5d6c90cda6af0d9acc709ca",
+    "url": "/MSA/static/media/class.baa6c7d2.jpg"
+  },
+  {
+    "revision": "fd602b34ae5d716e41e89d3d58e3fb0b",
+    "url": "/MSA/static/media/cuneyd.fd602b34.jpg"
+  },
+  {
+    "revision": "6b911764a172bae8c5ff082a87b8b4b3",
+    "url": "/MSA/static/media/fence.6b911764.jpg"
+  },
+  {
+    "revision": "9d42793d4b12e54ad16014c9354523dc",
+    "url": "/MSA/static/media/fundraiser.9d42793d.jpg"
+  },
+  {
+    "revision": "4263f6c86d868680a84725403407e3f7",
+    "url": "/MSA/static/media/gatherPic1.4263f6c8.jpg"
+  },
+  {
+    "revision": "6038d2e171bd14b901d4ffa584f11d60",
+    "url": "/MSA/static/media/jumuah.6038d2e1.jpg"
+  },
+  {
+    "revision": "5be46c52c6514db5085584825f839616",
+    "url": "/MSA/static/media/kaaba.5be46c52.jpg"
+  },
+  {
+    "revision": "cc97b1c3091219544baad061c9fee1c7",
+    "url": "/MSA/static/media/mansour.cc97b1c3.jpg"
+  },
+  {
+    "revision": "786f713005409571391a858619d297f9",
+    "url": "/MSA/static/media/mosque.786f7130.jpg"
+  },
+  {
+    "revision": "5883296c99a9b786dbe9919f1dea6eb2",
+    "url": "/MSA/static/media/msaLogo.5883296c.png"
+  },
+  {
+    "revision": "5ec419ea27bb657a35454a5ee1af67a4",
+    "url": "/MSA/static/media/quran.5ec419ea.jpg"
+  }
+]);
